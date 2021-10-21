ec1865222dbe62b77ead921374be4235d75f59d4
Ryan <omgitsgod@gmail.com>
2021-10-21 09:57:19 -0400

    Add check for yarn and add package to buildProject

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,26 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const hasbin = require('hasbin');
-const { askProject, askEnvironment } = require('./inquirer');
+const { askPackage, askProject, askEnvironment } = require('./inquirer');
 const { buildProject } = require('./builds')
 
 const run = async () => {
+
   const dockerIsInstalled = hasbin.sync('docker')
   const dockerComposeIsInstalled = hasbin.sync('docker-compose')
+  const yarnIsInstalled = hasbin.sync('yarn')
+  
   console.log(
     chalk.yellow(figlet.textSync('docker-it', { horizontalLayout: 'full' }))
   );
   console.log(`docker: ${dockerIsInstalled ? chalk.green('installed') : chalk.red('not installed')}`)
   console.log(`docker-compose: ${dockerComposeIsInstalled ? chalk.green('installed') : chalk.red('not installed')}`);
+  
+  const package = yarnIsInstalled ? await askPackage() : 'npm';
+  const project = await askProject();
+  const env = await askEnvironment();
 
-  const { project } = await askProject();
-  if (project === 'Exit') process.exit()
-  const { env } = await askEnvironment();
-  if (env === 'Exit') process.exit();
-
-  buildProject(project, env);  
+  buildProject(package, project, env);  
 };
 
-module.exports = { run }
+module.exports = { run };

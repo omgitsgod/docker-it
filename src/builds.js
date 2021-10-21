8aecb4312323367b8c67bf55ab9017511b5ba5d5
Ryan <omgitsgod@gmail.com>
2021-10-21 09:56:32 -0400

    Add package to check if yarn or npm

--- src/builds.js
+++ src/builds.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const buildReactDockerDev = () => {
+const buildReactDockerDev = (package) => {
   
   const dockerfileDevPath = './Dockerfile.dev';
   const dockerComposeDevPath = './docker-compose.dev.yml';
@@ -8,11 +8,12 @@ const buildReactDockerDev = () => {
   const nginxDirPath = './nginx';
   const nginxDevConfPath = './nginx/nginx.dev.conf';
   const packagePath = './package.json';
+  const yarn = package === 'yarn'
 
   const dockerFileDevContent =
-    'FROM node:16-slim\n\nWORKDIR /app\n\nCOPY package.json ./\n\nCOPY yarn.lock ./\n\nRUN yarn install --network-timeout 1000000\n\nCOPY . .\n';
+    `FROM node:16-slim\n\nWORKDIR /app\n\nCOPY package.json ./\n\nCOPY ${yarn ? 'yarn.lock' : 'package-lock.json'} ./\n\nRUN ${yarn ? 'yarn' : 'npm'} install --network-timeout 1000000\n\nCOPY . .\n`;
   const dockerComposeDevContent =
-    'version: "3"\n\nservices:\n\n  nginx:\n    container_name: core_web\n    restart: on-failure\n    image: nginx:stable\n    volumes:\n      - ./nginx/nginx.dev.conf:/etc/nginx/conf.d/default.conf\n    ports:\n      - "80:80"\n    depends_on:\n      - web\n  web:\n    container_name: react_app\n    restart: on-failure\n    build:\n      context: .\n      dockerfile: Dockerfile.dev\n    volumes:\n      - ./src:/app/src\n    ports:\n      - "3000:3000"\n    command: >\n      sh -c "yarn start"\n    environment:\n      - CI=true\n';
+    `version: "3"\n\nservices:\n\n  nginx:\n    container_name: core_web\n    restart: on-failure\n    image: nginx:stable\n    volumes:\n      - ./nginx/nginx.dev.conf:/etc/nginx/conf.d/default.conf\n    ports:\n      - "80:80"\n    depends_on:\n      - web\n  web:\n    container_name: react_app\n    restart: on-failure\n    build:\n      context: .\n      dockerfile: Dockerfile.dev\n    volumes:\n      - ./src:/app/src\n    ports:\n      - "3000:3000"\n    command: >\n      ${yarn ? 'sh -c "yarn start"' : 'sh -c "npm start"'}\n    environment:\n      - CI=true\n`;
   const dockerignoreContent =
     'node_modules\nnpm-debug.log\nDockerfile.dev\nDockerfile.prod\n.dockerignore\nyarn-error.log\n';
   const nginxDevConfContent =
@@ -56,10 +57,10 @@ const buildReactDockerDev = () => {
   });
 };
 
-const buildProject = (project, environment) => {
+const buildProject = (package, project, environment) => {
   switch (`${project}-${environment}`) {
     case 'React-Dev':
-      return buildReactDockerDev();
+      return buildReactDockerDev(package);
     default:
       console.log('build')
   }
